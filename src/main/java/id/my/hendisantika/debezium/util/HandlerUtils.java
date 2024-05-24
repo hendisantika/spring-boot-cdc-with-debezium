9abe4133002c5407467cb88181c017c2860b4a94
hendisantika <hendisantika@yahoo.co.id>
2024-05-25 05:45:45 +0700

    Add HandlerUtils🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/debezium/util/HandlerUtils.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.debezium.util;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-cdc-with-debezium
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/25/24
+ * Time: 05:45
+ * To change this template use File | Settings | File Templates.
+ */
+@UtilityClass
+public class HandlerUtils {
+
+}
